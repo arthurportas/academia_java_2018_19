bfcb237680a1bb930413092da70d3dadd01c462c
TiagoRpt <tig619@hotmail.com>
2018-12-05 19:58:29 +0000

    Develop_list_all_employees_with_salary_higher_than_x

--- db/src/main/java/service/DBServiceSalary.java
+++ db/src/main/java/service/DBServiceSalary.java
@@ -1,19 +1,15 @@
 package service;
 
 import db.DB;
-import dto.Employee_salary;
+import dto.Employee;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class DBService_salary {
+public class DBServiceSalary {
 
-
-
-
-    public List myList()  throws ClassNotFoundException, SQLException {
+    public List<Employee> myList()  throws ClassNotFoundException, SQLException {
 
       /*  Class.forName("com.mysql.cj.jdbc.Driver");
         String jdbc = "jdbc:mysql://localhost/employees?user=root&password=password";
@@ -30,11 +26,12 @@ public class DBService_salary {
 
 
         ResultSet listResultSet = statement.executeQuery(listSalaries);
+        List<Employee> salaryList = new ArrayList();
 
         while (listResultSet.next()) {
 
 
-            List salaryList = new ArrayList();
+
             salaryList.add((listResultSet.getInt("emp_no")));
             salaryList.add(listResultSet.getInt("salary"));
             salaryList.add(listResultSet.getString("from_date"));
@@ -46,7 +43,12 @@ public class DBService_salary {
         return null;
     }
 
+    public static void main(String[] args) {
 
+
+
+
+    }
 
 
 
