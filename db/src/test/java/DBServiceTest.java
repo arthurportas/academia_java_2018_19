4ed5f505bd701c717cbbc96577ebb50cc00e0b16
drekerd <marioalves.silva17@gmail.com>
2018-12-10 15:36:47 +0000

    properties_connection

--- db/src/test/java/DBServiceTest.java
+++ db/src/test/java/DBServiceTest.java
@@ -1,11 +1,16 @@
+import db.PropertiesConnection;
 import dto.Employee;
 import org.junit.Assert;
 import org.junit.Test;
 import service.DBService;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -73,6 +78,44 @@ public class DBServiceTest {
         Employee emp = employeeList.get(0);
         assertThat(emp).isEqualToIgnoringGivenFields(toSave, "number", "hireDate");
 
+    }
+
+    @Test
+    public void getProperties_returnResultTrue() throws SQLException, ClassNotFoundException, IOException {
+
+        PropertiesConnection propertiesConnection = new PropertiesConnection();
+
+        Properties prop = new Properties();
+        FileInputStream file;
+
+
+        try{
+            file = new FileInputStream("src/main/resources/db/db.properties");
+
+            if (file != null) {
+                prop.load(file);
+            } else {
+                throw new FileNotFoundException("property file not found in the classpath");
+            }
+
+            prop.load(file);
+
+            propertiesConnection.setDbuser(prop.getProperty("dbuser"));
+            propertiesConnection.setDbpasswrod(prop.getProperty("dbpassword"));
+            propertiesConnection.setDbase(prop.getProperty("dbase"));
+            propertiesConnection.setJdbc(prop.getProperty("jdbcDriver"));;
+
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        Assert.assertTrue(propertiesConnection.getDbuser().equals("root"));
+        Assert.assertTrue(propertiesConnection.getJdbc()
+                .equals("jdbc:mysql://localhost/employees?user=root&password=1909mm81"));
     }
 
 }
